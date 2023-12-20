42fdaf9e2dbabd1560cf92266fa95e8242753f69
yesidooo <hb25388@hb25388s-MacBook-Pro.local>
2023-12-20 20:12:58 +0800

    @llf

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A generalized deep learning training framework for image classification.
+A generalized deep learning training framework for image classification. This is a very simple implementation.
---
+++ dataset/aug_special.py
@@ -0,0 +1,32 @@
+import random
+
+from torchvision import transforms
+
+
+__all__ = [
+    'MixUp', 'CutMix', 'Fogging', 'RandomOffsetCrop'
+]
+
+
+class MixUp:
+
+    def __init__(self, ):
+        raise NotImplementedError()
+
+
+class CutMix:
+
+    def __init__(self, ):
+        raise NotImplementedError()
+
+
+class Fogging:
+
+    def __init__(self, ):
+        raise NotImplementedError()
+
+
+class RandomOffsetCrop:
+
+    def __init__(self, ):
+        raise NotImplementedError()
