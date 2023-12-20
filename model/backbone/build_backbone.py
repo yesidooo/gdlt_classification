1ac1c3041fa78f4a9424930321e5b70002e858e5
yesidooo <hb25388@hb25388s-MacBook-Pro.local>
2023-12-20 21:07:19 +0800

    some change

--- model/backbone/build_backbone.py
+++ model/backbone/build_backbone.py
@@ -1,6 +1,21 @@
-from .resnet import *
+import timm
+from torchvision import models
 
 
-def build_backbone(backbone_mode='resnet18', pretrained=True, pretrained_path=None):
-    if backbone_mode == 'resnet18':
-        return resnet18(pretrained, pretrained_path)
+def build_backbone(cfg):
+    """
+    根据配置文件加载 backbone (from torchvision or timm), 并根据配置加载预训练权重
+    :param cfg:
+    :return: model with/without pretrained weights
+    """
+    backbone = cfg.get('backbone', None)
+    pretrained = cfg.get('pretrained', False)
+    pretrained_path = cfg.get('pretrained_path', None)
+
+    if pretrained_path is not None:
+        pretrained = False
+
+    if hasattr(models, backbone):
+        backbone = getattr(models, backbone)(pretrained=pretrained)
+
+    raise NotImplementedError()
